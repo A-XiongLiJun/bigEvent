2163ec0d6e73da50ec6b749d891f2f2eff001efa
熊丽君 <416326495@qq.com>
2020-09-10 11:12:42 +0800

    登录注册表单验证完成

--- assets/js/login.js
+++ assets/js/login.js
@@ -6,4 +6,16 @@ $(function() {
     $('#showLogin').on('click', function() {
         $('.zc').hide().siblings().show()
     })
+    var form = layui.form
+    form.verify({
+        password: [
+            /^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'
+        ],
+        repassword: function(value) {
+            var pwd = $('.zc [name=password]').val()
+            if (pwd !== value) {
+                return '两次密码输入不一致！'
+            }
+        }
+    })
 })
