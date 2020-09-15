409b59de387269019eeb149d3607deaec914ef82
熊丽君 <416326495@qq.com>
2020-09-15 12:39:20 +0800

    文章列表完成

--- assets/js/article/art_list.js
+++ assets/js/article/art_list.js
@@ -1,5 +1,6 @@
 $(function() {
     var form = layui.form
+    var laypage = layui.laypage
     var q = {
         pagenum: 1, //页码值
         pagesize: 2, //每页显示多少条数据
@@ -8,7 +9,7 @@ $(function() {
     }
     initTable()
     initCate()
-        // 渲染文章标题
+        // 渲染文章数据table
     function initTable() {
         $.ajax({
             url: '/my/article/list',
@@ -19,6 +20,7 @@ $(function() {
                 }
                 var htmlStr = template('tpl-table', res)
                 $('tbody').html(htmlStr)
+                renderPage(res.total)
             }
         })
     }
@@ -56,11 +58,53 @@ $(function() {
     // 把值赋值给对象(q)里面对应的键
     // 发起ajax请求  渲染文章列表
     $('#formSearch').on('submit', function(e) {
-        e.preventDefault()
-        var cate_id = $('[name=cate_id]').val()
-        var state = $('[name=state]').val()
-        q.cate_id = cate_id
-        q.state = state
-        initTable() //渲染文章标题
+            e.preventDefault()
+            var cate_id = $('[name=cate_id]').val()
+            var state = $('[name=state]').val()
+            q.cate_id = cate_id
+            q.state = state
+            initTable() //渲染文章标题
+        })
+        // 渲染分页函数
+    function renderPage(total) {
+        laypage.render({
+            elem: 'renderPage', //配置id
+            count: total, //总条数
+            limit: q.pagesize, //每页的条数
+            curr: q.pagenum, //起始页
+            layout: ['count', 'limit', 'prev', 'page', 'next', 'skip'],
+            limits: [2, 3, 5, 10],
+
+            jump: function(obj, first) {
+                q.pagenum = obj.curr
+                if (!first) {
+                    q.pagesize = obj.limit
+                    initTable()
+                }
+            }
+        })
+    }
+    // 删除文章按钮事件
+    $('body').on('click', '.btn-delete', function() {
+        var len = $('.btn-delete').length
+        var id = $(this).attr('data-id')
+
+        layer.confirm('确认删除?', { icon: 3, title: '提示' }, function(index) {
+            $.ajax({
+                url: `/my/article/delete/${id}`,
+                success: function(res) {
+                    if (res.status !== 0) {
+                        return layer.msg(res.message)
+                    }
+                    layer.msg(res.message)
+                    if (len === 1) {
+                        q.pagenum = q.pagenum === 1 ? 1 : q.pagenum - 1
+                    }
+                    initTable()
+                }
+            })
+
+            layer.close(index);
+        });
     })
 })
